9f6f006344f3364b1929353e9ae775c192883dc3
Marie Dyrmose <mari48v8@stud.kea.dk>
2019-05-15 23:43:21 +0200

    Initial push to master

---
+++ front-page.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * The front page template file
+ *
+ * If the user has selected a static page for their homepage, this is what will
+ * appear.
+ * Learn more: https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+<script>
+
+$(function()
+{
+    window.onorientationchange = OrientationChanged;
+    window.setTimeout(OrientationChanged, 0);
+}
+function OrientationChanged(e)
+{
+    $('body').css('-webkit-transform', window.orientation % 180 == 0 ? '' : 'rotate(-90deg)');
+}
+
+</script>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+    <section>
+    <div class="container">
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+      ioafjaojfjaofja
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
+</section>
+
+	
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+get_footer();
